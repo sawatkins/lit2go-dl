eb211a83d996941d7aed98326973c302c40556b3
Simon Watkins <41589344+sawatkins@users.noreply.github.com>
2022-09-05 18:04:19 -0700

    clean up

--- lit2go-dl.py
+++ lit2go-dl.py
@@ -28,11 +28,9 @@ def get_mp3_link(url):
     soup = BeautifulSoup(r.content, "html.parser")
     for link in soup.find_all('source'):
         if link is not None:
-            # Why the fuck arent the ogg file getting here!?
             return link.get('src')
 
 base_url = input("Enter url of book from lit2go site: ")
-#test_base_url = "https://etc.usf.edu/lit2go/21/the-adventures-of-huckleberry-finn"
 book_name = base_url.strip('/').split('/')[-1]
 print("Found audiobook:", book_name)
 make_dir("./" + book_name)
@@ -43,9 +41,7 @@ for link in soup.find_all('a'):
     if link.get('href'):
         actual_link = link.get('href')
     if book_name in actual_link:
-        #print("actual_link:", actual_link)
         mp3_link = get_mp3_link(actual_link)
-        #print("mp3_link:", mp3_link)
         filename = actual_link.strip('/').split('/')[-1] + ".mp3"
         download_mp3(mp3_link, filename)
 
